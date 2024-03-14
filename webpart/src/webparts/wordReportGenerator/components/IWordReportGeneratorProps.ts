80909f20d43736cb9f1f04272703bae4830e8b19
Rene Köldorfer <rkoeldorfer@hotmail.com>
2024-03-14 21:47:25 +0100

    Initial Commit

---
+++ webpart/src/webparts/wordReportGenerator/components/IWordReportGeneratorProps.ts
@@ -0,0 +1,7 @@
+export interface IWordReportGeneratorProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
